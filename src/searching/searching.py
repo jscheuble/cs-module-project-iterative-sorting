e9e0ed6fa0d2570843b2e489416ac77841582995
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-07-20 19:35:02 -0700

    binary and linear search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for x in range(len(arr)):
+        if arr[x] == target:
+            return x
 
     return -1   # not found
 
@@ -9,6 +11,17 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    low = 0
+    high = len(arr) - 1
 
+    while low <= high:
+        middle = (high + low) // 2
+
+        if arr[middle] == target:
+            return middle
+        elif arr[middle] > target:
+            high = middle - 1
+        else:
+            low = middle + 1
 
     return -1  # not found
